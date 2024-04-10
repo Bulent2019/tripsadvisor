3d5e33393fbcf6dc604115cdd7da4d2906f7401d
Bülent <buelent_@hotmail.com>
2024-04-10 10:12:48 +0300

    5 jokes added

---
+++ application.js
@@ -0,0 +1,14 @@
+let cnJokes = [
+    "Time waits for no man. Unless that man is Chuck Norris.",
+    "If you spell Chuck Norris in Scrabble, you win. Forever.",
+    "Chuck Norris breathes air ... five times a day.",
+    "When God said, “Let there be light!” Chuck Norris said, “Say Please.”",
+    "Chuck Norris' tears cure cancer. Too bad he has never cried."
+];
+
+
+
+
+
+
+
